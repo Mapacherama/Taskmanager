d63a80da11589a6e9a4af63da5f0cd70c07e0972
Mapacherama <jerome_tesselaar@hotmail.com>
2022-01-24 10:49:35 +0100

    Test if this works

--- api/app.js
+++ api/app.js
@@ -150,6 +150,38 @@ app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
     });
 });
 
+/* USER ROUTES */
+
+/**
+ * POST /users
+ * Purpose: Sign up
+ */
+app.post('/users', (req, res) => {
+    // User sign up
+
+    let body = req.body;
+    let newUser = new User(body);
+
+    newUser.save().then(() => {
+        return newUser.createSession();
+    }).then((refreshToken) => {
+        // Session created successfully - refreshToken returned.
+        // now we geneate an access auth token for the user
+
+        return newUser.generateAccessAuthToken().then((accessToken) => {
+            // access auth token generated successfully, now we return an object containing the auth tokens
+            return { accessToken, refreshToken }
+        });
+    }).then((authTokens) => {
+        // Now we construct and send the response to the user with their auth tokens in the header and the user object in the body
+        res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(newUser);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log("John Parr, The first British soldier killed during WW1");
